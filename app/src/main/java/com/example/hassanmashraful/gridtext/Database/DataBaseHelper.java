7936090e7c1a7201471a8997179c3f8b56a5a16d
Hassan M.Ashraful <hassan963@outlook.com>
2016-09-21 02:14:15 +0600

    POS main acitivity done, adding into listview, deleting, add, minus, price

--- app/src/main/java/com/example/hassanmashraful/gridtext/Database/DataBaseHelper.java
+++ app/src/main/java/com/example/hassanmashraful/gridtext/Database/DataBaseHelper.java
@@ -12,10 +12,11 @@ import android.util.Log;
 public class DataBaseHelper extends SQLiteOpenHelper {
 
     private static final String POS_INFO_DB_NAME = "POSDB.DB";
-    private static final int POS_INFO_DB_VERSION = 1;
+    private static final int POS_INFO_DB_VERSION = 4;
 
-    private static final String TEMP_INFO_CREATE_QUERY = "CREATE TABLE "+ TempReportInfo.NewTempInfo.TEMPR_INFO_TABEL_NAME +"("+ TempReportInfo.NewTempInfo.Product_NAME+" TEXT NOT NULL,"+ TempReportInfo.NewTempInfo.Product_PRICE +" REAL NOT NULL,"+ TempReportInfo.NewTempInfo.Product_QUANTITY +" INTEGER NOT NULL,"+ TempReportInfo.NewTempInfo.Product_TOTAL +" REAL NOT NULL);";
+    private static final String TEMP_INFO_CREATE_QUERY = "CREATE TABLE " + TempReportInfo.NewTempInfo.TEMPR_INFO_TABEL_NAME + "(" + TempReportInfo.NewTempInfo.Product_ID + " INTEGER NOT NULL," + TempReportInfo.NewTempInfo.Product_NAME + " TEXT NOT NULL," + TempReportInfo.NewTempInfo.Product_PRICE + " REAL NOT NULL," + TempReportInfo.NewTempInfo.Product_QUANTITY + " INTEGER NOT NULL);";
 
+    private static final String RECEIPT_INFO_CREATE_QUERY = "CREATE TABLE " + TempReportInfo.ReceiptInfo.RECEIPT_INFO_TABLE_NAME + "(" + TempReportInfo.ReceiptInfo.Receipt_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + TempReportInfo.ReceiptInfo.Pro_ID + " INTEGER NOT NULL," + TempReportInfo.ReceiptInfo.Receipt_Date + " TEXT NOT NULL," + TempReportInfo.ReceiptInfo.Receipt_Month + " TEXT NOT NULL," + TempReportInfo.ReceiptInfo.Receipt_Year + " TEXT NOT NULL," + TempReportInfo.ReceiptInfo.Receipt_Time + " TEXT NOT NULL," + TempReportInfo.ReceiptInfo.Pro_Total + " REAL NOT NULL);";
 
 
 
@@ -26,6 +27,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(TEMP_INFO_CREATE_QUERY);
+        db.execSQL(RECEIPT_INFO_CREATE_QUERY);
         Log.e("DB :::: ::: ::", "CREATED :::");
 
     }
@@ -33,21 +35,39 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS "+TempReportInfo.NewTempInfo.TEMPR_INFO_TABEL_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TempReportInfo.ReceiptInfo.RECEIPT_INFO_TABLE_NAME);
         onCreate(db);
 
     }
 
 
-    public void addTempTableInfo(SQLiteDatabase db, String p_name, float p_price, int p_quantity, float p_total){
+    public void addTempTableInfo(SQLiteDatabase db, int p_key, String p_name, float p_price, int p_quantity) {
 
         ContentValues contentValues = new ContentValues();
+
+        contentValues.put(TempReportInfo.NewTempInfo.Product_ID, p_key);
         contentValues.put(TempReportInfo.NewTempInfo.Product_NAME, p_name);
         contentValues.put(TempReportInfo.NewTempInfo.Product_PRICE, p_price);
         contentValues.put(TempReportInfo.NewTempInfo.Product_QUANTITY, p_quantity);
-        contentValues.put(TempReportInfo.NewTempInfo.Product_TOTAL, p_total);
 
         db.insert(TempReportInfo.NewTempInfo.TEMPR_INFO_TABEL_NAME, null, contentValues);
+        //db.close();
 
+    }
+
+
+    public void addReceiptTableInfo(SQLiteDatabase db, int p_key, String date, String month, String year, String time, Float p_total) {
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(TempReportInfo.ReceiptInfo.Pro_ID, p_key);
+        contentValues.put(TempReportInfo.ReceiptInfo.Receipt_Date, date);
+        contentValues.put(TempReportInfo.ReceiptInfo.Receipt_Month, month);
+        contentValues.put(TempReportInfo.ReceiptInfo.Receipt_Year, year);
+        contentValues.put(TempReportInfo.ReceiptInfo.Receipt_Time, time);
+        contentValues.put(TempReportInfo.ReceiptInfo.Pro_Total, p_total);
+
+        db.insert(TempReportInfo.ReceiptInfo.RECEIPT_INFO_TABLE_NAME, null, contentValues);
+        //db.close();
 
     }
 
--- app/src/main/java/com/example/hassanmashraful/gridtext/Database/TempReportInfo.java
+++ app/src/main/java/com/example/hassanmashraful/gridtext/Database/TempReportInfo.java
@@ -7,11 +7,24 @@ public class TempReportInfo {
 
     public static abstract class NewTempInfo{
 
+        public static final String Product_ID = "product_id";
         public static final String Product_NAME = "product_name";
         public static final String Product_QUANTITY = "product_quantity";
         public static final String Product_PRICE = "product_price";
         public static final String Product_TOTAL = "product_total";
         public static final String TEMPR_INFO_TABEL_NAME = "temp_table_info";
 
+    }
+
+
+    public static abstract class ReceiptInfo {
+        public static final String Receipt_ID = "r_id";
+        public static final String Pro_ID = "p_id";
+        public static final String Receipt_Date = "r_date";
+        public static final String Receipt_Month = "r_month";
+        public static final String Receipt_Year = "r_year";
+        public static final String Receipt_Time = "r_time";
+        public static final String Pro_Total = "receipt_total";
+        public static final String RECEIPT_INFO_TABLE_NAME = "receipt_table_info";
     }
 }
