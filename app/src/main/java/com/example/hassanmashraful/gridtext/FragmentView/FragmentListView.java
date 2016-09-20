964802a40455d7248ce92f2b1af2a27ddce55f5f
Hassan M.Ashraful <hassan963@outlook.com>
2016-09-21 02:11:39 +0600

    POS main acitivity done, adding into listview, deleting, add, minus, price

--- app/src/main/java/com/example/hassanmashraful/gridtext/FragmentView/FragmentListView.java
+++ app/src/main/java/com/example/hassanmashraful/gridtext/FragmentView/FragmentListView.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -18,10 +19,12 @@ import android.widget.Toast;
 
 import com.example.hassanmashraful.gridtext.Adapter.ListAdapter;
 import com.example.hassanmashraful.gridtext.Adapter.SwapListItemDelete;
+import com.example.hassanmashraful.gridtext.Database.DataBaseHelper;
 import com.example.hassanmashraful.gridtext.R;
 import com.example.hassanmashraful.gridtext.Receipt_Food;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 /**
  * Created by Hassan M.Ashraful on 8/24/2016.
@@ -31,7 +34,10 @@ public class FragmentListView extends Fragment {
     private RecyclerView recyclerView;
     private ArrayList<Receipt_Food> receipt_foods = new ArrayList<>();
     private ListAdapter adapter_list;
-    Float sum = 0f;
+    private int count = 1;
+    DataBaseHelper dataBaseHelper;
+    SQLiteDatabase sqLiteDatabase;
+
 
     @Nullable
     @Override
@@ -52,14 +58,15 @@ public class FragmentListView extends Fragment {
         ItemTouchHelper helper = new ItemTouchHelper(callback);
         helper.attachToRecyclerView(recyclerView);
 
-        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(mMessageReceiver,
+        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(mGridMessageReceiver,
                 new IntentFilter("grid-message"));
+
 
         return rootView;
     }
 
 
-    public BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+    public BroadcastReceiver mGridMessageReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             // Get extra data included in the Intent
@@ -76,19 +83,89 @@ public class FragmentListView extends Fragment {
 
         receipt_foods.add(new Receipt_Food(name, quantity, price));
 
+
     }
 
     public ArrayList<Receipt_Food> getReceipt_foods() {
-        Toast.makeText(getActivity(), receipt_foods.size() + " SIZE", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getActivity(), receipt_foods.size() + " SIZE", Toast.LENGTH_SHORT).show();
 
 
         return receipt_foods;
 
     }
 
+
+    public void showFOOD() {
+        int sum = 0, p_quantity;
+        Float p_price, p_total = null;
+        String p_name;
+
+        Calendar c = Calendar.getInstance();
+
+        int seconds = c.get(Calendar.SECOND);
+        int minutes = c.get(Calendar.MINUTE);
+        int hour = c.get(Calendar.HOUR);
+        String time = hour + ":" + minutes + ":" + seconds;
+
+
+        String day = String.valueOf(c.get(Calendar.DAY_OF_MONTH));
+        String month = String.valueOf(c.get(Calendar.MONTH) + 1);
+        String year = String.valueOf(c.get(Calendar.YEAR));
+        //String date = day+"/"+month+"/"+year;
+
+        dataBaseHelper = new DataBaseHelper(getActivity());
+        sqLiteDatabase = dataBaseHelper.getWritableDatabase();
+
+        for (int i = 0; i < receipt_foods.size(); i++) {
+
+            //Log.v("DATA IN :: ", receipt_foods.get(i).getFoodName() + " " + receipt_foods.get(i).getFoodQuantity() + " " + receipt_foods.get(i).getFoodPrice());
+            //Toast.makeText(getActivity(), receipt_foods.get(i).getFoodName() + " " + receipt_foods.get(i).getFoodQuantity() + " " + receipt_foods.get(i).getFoodPrice(), Toast.LENGTH_SHORT).show();
+            sum += Integer.parseInt(receipt_foods.get(i).getShowPrice());
+
+            p_price = Float.parseFloat(receipt_foods.get(i).getShowPrice());
+            p_quantity = Integer.parseInt(receipt_foods.get(i).getShowQuantity());
+            p_total = Float.valueOf(sum);
+            p_name = receipt_foods.get(i).getFoodName();
+
+            //int p_key, String p_name, float p_price, int p_quantity, float p_total
+            dataBaseHelper.addTempTableInfo(sqLiteDatabase, count, p_name, p_price, p_quantity);
+
+
+        }
+        //int p_key, String date, String month, String year, String time, Float p_total
+        dataBaseHelper.addReceiptTableInfo(sqLiteDatabase, count, day, month, year, time, p_total);
+        dataBaseHelper.close();
+        count++;
+        Toast.makeText(getActivity(), "Total ::::" + sum, Toast.LENGTH_SHORT).show();
+        //receipt_foods.clear();
+
+    }
+
+
+   /* public void dateTimeFormat(){
+        Calendar c = Calendar.getInstance();
+
+        int seconds = c.get(Calendar.SECOND);
+        int minutes = c.get(Calendar.MINUTE);
+        int hour = c.get(Calendar.HOUR);
+        String time = hour+":"+minutes+":"+seconds;
+
+
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int month = c.get(Calendar.MONTH)+1;
+        int year = c.get(Calendar.YEAR);
+        String date = day+"/"+month+"/"+year;
+
+        Toast.makeText(getActivity(), date + " " + time, Toast.LENGTH_LONG).show();
+        Log.e("DATE  ", date);
+
+    }*/
+
+
     public void updateInfo() {
         //setReceipt_foods(name, "1", price);
         recyclerView.setAdapter(adapter_list);
+        adapter_list.notifyDataSetChanged();
         //sum = sumMyIntValues(listView);
     }
 /*
